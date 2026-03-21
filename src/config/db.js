88bc3b6a6cc2d33b0ac19a437a946f72aba96cb1
Manoranjan D Salunke <manoranjan710@gmail.com>
2026-03-21 22:08:39 +0530

    SSL removed

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,6 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
      uri: process.env.DATABASE_URL,
      connectionLimit: 10,
-     ssl: {
-    rejectUnauthorized: false
-  }
 });
 
 module.exports = pool;
