64752df91d49ee42e5a065164fe7ca945f6c43c2
Manoranjan D Salunke <manoranjan710@gmail.com>
2026-03-21 21:52:01 +0530

    SSL authorization

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,10 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
      uri: process.env.DATABASE_URL,
-     connectionLimit: 10
+     connectionLimit: 10,
+     ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = pool;
