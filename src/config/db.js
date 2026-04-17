eb5b25e5037f06d987084ab5c3de8fbf3c153f33
Manoranjan D Salunke <manoranjan710@gmail.com>
2026-04-17 12:22:00 +0530

    DB: Db connection migrated to AIVEN

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-     uri: process.env.DATABASE_URL,
-     connectionLimit: 10,
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    ca: process.env.DB_SSL_CA,
+  },
+  connectionLimit: 10,
 });
 
 module.exports = pool;
